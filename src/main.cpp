986d596ef4297aee03f552e3d7c19a770064649b
doviliukasss <stakyte.d@gmail.com>
2026-01-28 16:20:42 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -23,12 +23,15 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883
 #define AIO_USERNAME    "doviliukas_1"
-#define AIO_KEY         ""   // <- TAVO ADAFRUIT IO KEY
+#define AIO_KEY         ""   // <-- TAVO ADAFRUIT IO KEY
 
 // ===== DHT11 =====
 #define DHTPIN 27
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
+
+// ===== RELAY =====
+#define RELAY_PIN 26
 
 // ===== MQTT =====
 WiFiClient client;
@@ -44,11 +47,14 @@ Adafruit_MQTT_Publish humidityFeed =
 unsigned long lastReadTime = 0;
 const unsigned long readInterval = 5000;
 
+unsigned long lastRelayTime = 0;
+
 // ===== DATA =====
 float temperature = 0;
 float humidity = 0;
 
 bool wifiOK = false;
+bool relayState = false;
 
 // ===== MQTT CONNECT =====
 void MQTT_connect() {
@@ -64,8 +70,8 @@ void MQTT_connect() {
 void setup() {
   Serial.begin(9600);
 
+  // OLED I2C
   Wire.begin(21, 22);
-
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
     while (true);
   }
@@ -77,19 +83,24 @@ void setup() {
   display.println("ESP32 paleistas");
   display.display();
 
+  // DHT
   dht.begin();
 
-  // ===== WiFi (neblokuojantis) =====
+  // Relay
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW); // rėlė išjungta
+
+  // WiFi (neblokuojantis)
   WiFi.begin(WLAN_SSID, WLAN_PASS);
 }
 
 // ================= LOOP =================
 void loop() {
 
-  // ===== Tikrinam WiFi =====
+  // ===== WiFi būsena =====
   wifiOK = (WiFi.status() == WL_CONNECTED);
 
-  // ===== Skaitymas kas 5 sek =====
+  // ===== DHT skaitymas kas 5 s =====
   if (millis() - lastReadTime >= readInterval) {
     lastReadTime = millis();
 
@@ -115,6 +126,13 @@ void loop() {
       temperatureFeed.publish(temperature);
       humidityFeed.publish(humidity);
     }
+  }
+
+  // ===== RELAY kas 1 sek =====
+  if (millis() - lastRelayTime >= 1000) {
+    lastRelayTime = millis();
+    relayState = !relayState;
+    digitalWrite(RELAY_PIN, !relayState);
   }
 
   // ===== OLED =====
